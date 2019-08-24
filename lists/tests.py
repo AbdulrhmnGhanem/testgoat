b0ddfe169553fe076d06eef8d2d1c12f16525643
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-08-24 02:36:18 +0200

    update the dummy view(still dummy), and its tests

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpResponse
+
 from .views import homepage
 
 
@@ -8,6 +10,13 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_homepage_view(self):
         found = resolve('/')
         self.assertEqual(found.func, homepage)
+
+    def test_homepage_returns_correct_html(self):
+        request = HttpResponse()
+        response = homepage(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
 
 
 class SmokeTest(TestCase):
