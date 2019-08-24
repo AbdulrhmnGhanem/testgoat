ff9809735ab537656a264592b6d4f7f68a0eef65
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-08-24 18:45:29 +0200

    Functional test update

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 
 
 def homepage(request):
-
-    return render(request, 'lists/homepage.html')
+    return render(request, 'lists/homepage.html', {
+        'new_item_text': request.POST.get('item_text', '')
+    })
