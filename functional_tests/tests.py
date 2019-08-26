3b5196186e4a570885ca782d2a07b46c54a5e18d
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-08-26 06:47:31 +0200

    +Bootstrap style to some elements

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,7 @@
 import time
 
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
@@ -9,7 +10,7 @@ from selenium.webdriver.common.keys import Keys
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTes(StaticLiveServerTestCase):
 
     def setUp(self) -> None:
         self.browser = webdriver.Chrome()
@@ -115,11 +116,15 @@ class NewVisitorTest(LiveServerTestCase):
         # Satisfied, they both go back to sleep
 
     def test_layout_and_styling(self):
+
         # Edith goes to the home page
         self.browser.get(self.live_server_url)
+        # the `self.live_server_url` breaks static files
         self.browser.set_window_size(1024, 768)
+
         # She notices the input box is nicely centered
         inputbox = self.browser.find_element_by_id('id_new_item')
+
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
@@ -127,7 +132,6 @@ class NewVisitorTest(LiveServerTestCase):
 
         # she starts a new list and see that the input is nicely
         # centered there too
-
         inputbox.send_keys('testing\n')
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
