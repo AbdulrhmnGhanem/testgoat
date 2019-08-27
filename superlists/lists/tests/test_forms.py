77403cb9bdfb5dc4d38edf2d5684adbfbed6d55e
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-08-27 21:10:33 +0200

    new form for list items

---
+++ superlists/lists/tests/test_forms.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+from ..forms import ItemForm, EMPTY_LIST_ERROR
+
+
+class ItemFormTest(TestCase):
+
+    def test_from_renders_item_text_input(self):
+
+        form = ItemForm()
+        self.assertIn('placeholder="Enter a to-do item"', form.as_p())
+        self.assertIn('class="form-control input-group-lg"', form.as_p())
+
+    def test_form_validation_for_blank_item(self):
+
+        form = ItemForm(data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'],[EMPTY_LIST_ERROR])
